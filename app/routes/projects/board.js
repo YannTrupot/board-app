dace5b4704827323438bbaa912fb4bb19406dbed
Yann <21601207@etu.unicaen.fr>
2018-04-04 20:55:52 +0200

    Mise en commentaire du link-to projects.board qui fait planter la page d'affichage des stories d'un projet

--- app/routes/projects/board.js
+++ app/routes/projects/board.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
+import RSVP from "rsvp";
 
 export default Route.extend({
+  model(params) {
+    return RSVP.hash({
+      story: EmberObject.create(),
+      project: this.get('store').findRecord('project', params.project_id)
+    });
+  }
+
 });
